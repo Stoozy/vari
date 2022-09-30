25738502365aff5d82001e8700be9619d2b5bc6d
stoozy <stoozy@proton.me>
2022-09-29 23:29:39 -0400

    Moved vari impl to `vari.rs`

--- src/main.rs
+++ src/main.rs
@@ -5,62 +5,10 @@ mod expr;
 mod tests;
 
 use std::env;
-use std::fs;
-
-use crate::lexer::Lexer;
-use vari::Vari;
 use vari::VARI;
 
-impl Vari {
-    fn report(&self, line: usize, location: &str, msg: &str) -> () {
-        println!("Error on line {}:  {} {}", line, location, msg);
-    }
-
-    fn run(&self, source: &str) {
-        println!("Source: {}", source);
-        let mut lexer: Lexer = Lexer::new(source.to_owned());
-
-        let tokens = lexer.scan_tokens();
-        let n_tokens = tokens.len();
-
-        for token in tokens {
-            println!("{}", token.to_string());
-        }
-
-        println!("Scanned {} tokens", n_tokens);
-
-        if self.had_error {
-            std::process::exit(1);
-        }
-    }
-
-    fn read_source(&self, file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
-        let data = fs::read_to_string(file_path)?;
-        Ok(data)
-    }
-
-    pub fn error(&self, line: usize, msg: &str) {
-        self.report(line, "", msg);
-    }
-
-    pub fn run_prompt(&self) -> () {
-        loop {}
-    }
-
-    pub fn run_file(&self, file_path: &str) {
-        match self.read_source(file_path) {
-            Ok(data) => {
-                self.run(data.as_str());
-            }
-            Err(_) => todo!(),
-        }
-    }
-}
 
 fn main() {
-
-
-
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 2 {
--- src/vari.rs
+++ src/vari.rs
@@ -1,5 +1,54 @@
+use crate::lexer::Lexer;
+use std::fs;
 pub static VARI: Vari = Vari { had_error: false };
 
 pub struct Vari {
     pub had_error: bool,
+}
+
+
+impl Vari {
+    fn report(&self, line: usize, location: &str, msg: &str) -> () {
+        println!("Error on line {}:  {} {}", line, location, msg);
+    }
+
+    fn run(&self, source: &str) {
+        println!("Source: {}", source);
+        let mut lexer: Lexer = Lexer::new(source.to_owned());
+
+        let tokens = lexer.scan_tokens();
+        let n_tokens = tokens.len();
+
+        for token in tokens {
+            println!("{}", token.to_string());
+        }
+
+        println!("Scanned {} tokens", n_tokens);
+
+        if self.had_error {
+            std::process::exit(1);
+        }
+    }
+
+    fn read_source(&self, file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
+        let data = fs::read_to_string(file_path)?;
+        Ok(data)
+    }
+
+    pub fn error(&self, line: usize, msg: &str) {
+        self.report(line, "", msg);
+    }
+
+    pub fn run_prompt(&self) -> () {
+        loop {}
+    }
+
+    pub fn run_file(&self, file_path: &str) {
+        match self.read_source(file_path) {
+            Ok(data) => {
+                self.run(data.as_str());
+            }
+            Err(_) => todo!(),
+        }
+    }
 }
