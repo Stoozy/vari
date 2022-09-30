17216cf13e760cd962bc63cbcd858560ab4f1486
stoozy <stoozy@proton.me>
2022-09-29 23:27:31 -0400

    Added way for testing modules

---
+++ src/tests.rs
@@ -0,0 +1,27 @@
+
+#[cfg(test)]
+mod Tests {
+
+    #[test]
+    pub fn lexer_test(){
+
+        use crate::expr::AstPrinter;
+        use crate::expr::{Expr, VariTypes};
+        use crate::token::{Token, TokenType};
+
+        let mut printer: AstPrinter = AstPrinter::new();
+
+
+        let expr = Expr::Binary { 
+            lhs: Box::new( Expr::Unary { 
+                op: Token{token_type: TokenType::MINUS, lexeme: "-".to_owned(),literal: None, line: 1}, 
+                rhs: Box::new(Expr::Literal { value: Box::new(VariTypes::Num(25.0)) }) }), 
+            op: Token { token_type: TokenType::STAR, lexeme: "*".to_owned(), literal: None, line:1}, 
+            rhs: Box::new(Expr::Grouping { 
+                expr: Box::new(Expr::Literal { value: Box::new(VariTypes::Num(45.67)) }) 
+            }) 
+        };
+
+        assert!("( * ( - 25) ( group 45.67))" == printer.print(expr));
+    }
+}
